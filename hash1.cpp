a43b36051f6bea5c6f28c1247e573d8ec5424f47
Bharati patil <bp23092004@gmail.com>
2024-12-12 15:34:39 +0530

    Add files via upload

---
+++ hash1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"enter the number of elements in array :";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	int hash[13]={0};
+	for(int i =0;i<=n;i++)
+	{
+		hash[arr[i]]+=1;
+	}
+	
+	int q;
+	cout<<"enter the queries:";
+	cin>>q;
+	while(q--)
+	{
+		int number;
+		cout<<"enter the number to find how many times it occurs";
+		cin>>number;
+		
+		cout<<hash[number]<<endl;
+	}
+	return 0;
+}
